b184dff927f85a9140dc2ed296955af60b188838
mobius29 <sagjin0000@naver.com>
2024-09-15 13:30:24 +0900

    rfc: 폴더 리팩토링

--- reminder/main.py
+++ reminder/main.py
@@ -1,4 +1,3 @@
-import argparse
 import re
 
 from datetime import datetime
@@ -7,38 +6,21 @@ from discord.ext import commands
 from .config import DISCORD_BOT_TOKEN
 from .bot import bot
 from .utils import set_timeout
+from .parser import parser, set_parser, list_parser
+from .constants import NO_ARGS
+
 
 @bot.command()
 async def reminder(ctx: commands.Context, *args):
     if len(args) == 0:
-        await ctx.send("""
-최소 1개의 인자가 필요합니다.
-
-도움이 필요하다면 다음과 같이 입력하세요.
-`$reminder -h` 또는 `$reminder --help`
-""")
+        await ctx.send(NO_ARGS)
         return
 
-    parser = argparse.ArgumentParser(description="Discord Reminder Bot", add_help=False)
-    parser.add_argument("-h", "--help", action="store_true", dest="_help", help="도움말을 출력합니다.")
-
-    subparsers = parser.add_subparsers(dest="subcommand", help="리마인더 명령어를 입력하세요.")
-
-    set_parser = subparsers.add_parser("set", help="리마인더를 설정합니다.", add_help=False)
-    set_parser.add_argument("-h", "--help", action="store_true", dest="_set_help", help="도움말을 출력합니다.")
-    set_parser.add_argument("-m", "--message", dest="message", nargs="+", help="리마인더 메시지를 입력하세요.")
-    set_parser.add_argument("-w", "--week", dest="week", nargs="+", help="요일을 설정합니다. 기본적으로 모든 평일로 설정합니다.")
-    set_parser.add_argument("-d", "--date", dest="date", nargs="+", help="날짜를 설정합니다. 기본적으로 오늘로 설정합니다.")
-    set_parser.add_argument("-t", "--time", dest="time", nargs="+", help="시간을 설정합니다. 기본적으로 09:00:00로 설정합니다.")
-    set_parser.add_argument("--after", dest="after", nargs="+", help="리마인더를 설정한 시간 이후로 설정합니다. 이는 다른 시간 설정과 함께 사용할 수 없습니다.")
-
-    list_parser = subparsers.add_parser("list", help="리마인더 목록을 출력합니다.", add_help=False)
-    list_parser.add_argument("-h", "--help", action="store_true", dest="_list_help", help="도움말을 출력합니다.")
-    list_parser.add_argument("--author", dest="author", help="리마인더 목록을 출력할 사용자를 지정합니다.")
-
     me = ctx.me
-    channel = ctx.channel
-    channel_name = channel.name # type: ignore 
+    channel_name: str | None = getattr(ctx.channel, "name", None)
+    if not channel_name:
+        await ctx.send("채널이 아닌 곳에는 보낼 수 없습니다.")
+        return
 
     try:
         arguments = parser.parse_args(list(args))
@@ -56,32 +38,42 @@ async def reminder(ctx: commands.Context, *args):
                 await ctx.send(set_parser.format_help())
                 return
 
-            retMessage = f"{channel_name} 채널에 {me.name}님의 리마인더가 설정되었습니다."
+            retMessage = (
+                f"{channel_name} 채널에 {me.name}님의 리마인더가 설정되었습니다."
+            )
             if arguments.after is not None:
-                if arguments.week is not None or arguments.date is not None or arguments.time is not None:
-                    await ctx.send("--after 옵션은 다른 시간 설정과 함께 사용할 수 없습니다.")
+                if (
+                    arguments.week is not None
+                    or arguments.date is not None
+                    or arguments.time is not None
+                ):
+                    await ctx.send(
+                        "--after 옵션은 다른 시간 설정과 함께 사용할 수 없습니다."
+                    )
                     return
 
-                re_times = re.compile('(\\d+)(h|m|s)$')
-                times: list[str] = list(filter(lambda x: len(x) != 0, arguments.after[0].split(" ")))
+                re_times = re.compile("(\\d+)(h|m|s)$")
+                times: list[str] = list(
+                    filter(lambda x: len(x) != 0, arguments.after[0].split(" "))
+                )
 
                 time_text = []
                 seconds = 0
                 for time in times:
                     time = re_times.match(time)
-                    if time == None:
+                    if time is None:
                         raise Exception("Invalid time format")
 
                     (value, unit) = time.groups()
                     value = int(value)
 
-                    if unit == 'h':
+                    if unit == "h":
                         time_text.append(f"{value}시간")
                         seconds += value * 3600
-                    if unit == 'm':
+                    if unit == "m":
                         time_text.append(f"{value}분")
                         seconds += value * 60
-                    if unit == 's':
+                    if unit == "s":
                         time_text.append(f"{value}초")
                         seconds += value
 
@@ -91,11 +83,19 @@ async def reminder(ctx: commands.Context, *args):
 
                 return
 
-
-            date = arguments.date[0] if arguments.date is not None else datetime.now().strftime("%Y-%m-%d")
+            date = (
+                arguments.date[0]
+                if arguments.date is not None
+                else datetime.now().strftime("%Y-%m-%d")
+            )
             time = arguments.time[0] if arguments.time is not None else "09:00:00"
 
             date_time = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M:%S")
+            if date_time < datetime.now():
+                await ctx.send("현재 시간 이후로 설정해주세요.")
+                return
+
+            await ctx.send(date_time.ctime())
 
             return
 
@@ -111,13 +111,14 @@ async def reminder(ctx: commands.Context, *args):
             await ctx.send(parser.format_help())
             return
 
-
     except SystemExit as e:
         await ctx.send(parser.format_help())
         return
 
+
 def main():
     bot.run(DISCORD_BOT_TOKEN)
+
 
 if __name__ == "__main__":
     main()
---
+++ reminder/parser.py
@@ -0,0 +1,20 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Discord Reminder Bot", add_help=False)
+parser.add_argument("-h", "--help", action="store_true", dest="_help", help="도움말을 출력합니다.")
+
+_subparsers = parser.add_subparsers(dest="subcommand", help="리마인더 명령어를 입력하세요.")
+
+set_parser = _subparsers.add_parser("set", help="리마인더를 설정합니다.", add_help=False)
+set_parser.add_argument("-h", "--help", action="store_true", dest="_set_help", help="도움말을 출력합니다.")
+set_parser.add_argument("-m", "--message", dest="message", nargs="+", help="리마인더 메시지를 입력하세요.")
+set_parser.add_argument("-w", "--week", dest="week", nargs="+", help="요일을 설정합니다. 기본적으로 모든 평일로 설정합니다.")
+set_parser.add_argument("-d", "--date", dest="date", nargs="+", help="날짜를 설정합니다. 기본적으로 오늘로 설정합니다.")
+set_parser.add_argument("-t", "--time", dest="time", nargs="+", help="시간을 설정합니다. 기본적으로 09:00:00로 설정합니다.")
+set_parser.add_argument("--after", dest="after", nargs="+", help="리마인더를 설정한 시간 이후로 설정합니다. 이는 다른 시간 설정과 함께 사용할 수 없습니다.")
+
+list_parser = _subparsers.add_parser("list", help="리마인더 목록을 출력합니다.", add_help=False)
+list_parser.add_argument("-h", "--help", action="store_true", dest="_list_help", help="도움말을 출력합니다.")
+list_parser.add_argument("--author", dest="author", help="리마인더 목록을 출력할 사용자를 지정합니다.")
+
+__all__ = ['parser', 'set_parser', 'list_parser']
