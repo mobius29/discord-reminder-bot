4239970e6e3ee376623b0aaa971089f0b28bc21c
mobius29 <sagjin0000@naver.com>
2024-09-15 17:12:37 +0900

    feat: get reminder list from notion

--- reminder/commands/sub/list/service.py
+++ reminder/commands/sub/list/service.py
@@ -1,18 +1,10 @@
-from typing import TypedDict
 from argparse import Namespace
 from discord.ext import commands
+from datetime import datetime
 
 from reminder.config import NOTION_REMINDERS_DB_ID
 from reminder.notion import notion
 from .parser import list_parser
-
-
-class NotionDatabase(TypedDict):
-    object: str
-    id: str
-    created_time: str
-    last_edited_time: str
-    title: list[dict[str, str]]
 
 
 async def cmd_list(ctx: commands.Context, args: Namespace):
@@ -21,6 +13,15 @@ async def cmd_list(ctx: commands.Context, args: Namespace):
         return
 
     response = await notion.databases.query(database_id=NOTION_REMINDERS_DB_ID)
+    pages = response["results"]
 
-    for result in response["results"]:
-        print(result)
+    for page in pages:
+        properties = page["properties"]
+        id = properties["ID"]["unique_id"]["number"]
+        date = properties["날짜"]["date"]["start"]
+        creator = properties["생성자"]["created_by"]["name"]
+        message = properties["메시지"]["title"][0]["text"]["content"]
+
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f%z")
+        date = datetime.strftime(date, "%Y-%m-%d %H:%M")
+        await ctx.send(f"{id}\t{date}\t{message}\t{creator}")
